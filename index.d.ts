6398e880a069b097d7c90091cd45cdd31d4bb26d
Alex Hultman <alexhultman@gmail.com>
2019-03-10 14:18:37 +0100

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -5,42 +5,51 @@ interface us_listen_socket {
 
 }
 
+/** Recognized string types, things C++ can read and understand as strings */
+type RecognizedString = string | ArrayBuffer | Uint8Array | Int8Array | Uint16Array | Int16Array | Uint32Array | Int32Array | Float32Array | Float64Array;
+
 /** A WebSocket connection that is valid from open to close event */
 interface WebSocket {
     /** Sends a message. Make sure to check getBufferedAmount() before sending. Returns true for success, false for built up backpressure that will drain when time is given. */
-    send(message: string | ArrayBuffer, isBinary: boolean, compress: boolean) : boolean;
+    send(message: RecognizedString, isBinary: boolean, compress: boolean) : boolean;
 
     /** Returns the bytes buffered in backpressure. */
     getBufferedAmount() : number;
 
     /** Gradefully closes this WebSocket. Immediately calls close handler. */
-    end(code: number, shortMessage: string | ArrayBuffer) : WebSocket;
+    end(code: number, shortMessage: RecognizedString) : WebSocket;
 
     /** Forefully closes this WebSocket. Immediately calls close handler. */
     close() : WebSocket;
 
     /** Subscribe to a topic in MQTT syntax */
-    subscribe(topic: string | ArrayBuffer) : WebSocket;
+    subscribe(topic: RecognizedString) : WebSocket;
 
     /** Publish a message to a topic in MQTT syntax */
-    publish(topic: string | ArrayBuffer, message: string | ArrayBuffer) : WebSocket;
+    publish(topic: RecognizedString, message: RecognizedString) : WebSocket;
 
     /** Unsubscribe from topic (not implemented yet) */
-    unsubscribe(topic: string | ArrayBuffer) : WebSocket;
+    unsubscribe(topic: RecognizedString) : WebSocket;
+
+    /** Returns the remote IP address */
+    getRemoteAddress() : ArrayBuffer;
+    
+    /** Arbitrary user data may be attached to this object */
+    [key: string]: any;
 }
 
 /** An HttpResponse is valid until either onAborted callback or any of the .end/.tryEnd calls succeed. You may attach user data to this object. */
 interface HttpResponse {
     /** Writes the HTTP status message such as "200 OK". */
-    writeStatus(status: string | ArrayBuffer) : HttpResponse;
+    writeStatus(status: RecognizedString) : HttpResponse;
     /** Writes key and value to HTTP response. */
-    writeHeader(key: string | ArrayBuffer, value: string | ArrayBuffer) : HttpResponse;
+    writeHeader(key: RecognizedString, value: RecognizedString) : HttpResponse;
     /** Enters or continues chunked encoding mode. Writes part of the response. End with zero length write. */
-    write(chunk: string | ArrayBuffer) : HttpResponse;
+    write(chunk: RecognizedString) : HttpResponse;
     /** Ends this response by copying the contents of body. */
-    end(body: string | ArrayBuffer) : HttpResponse;
+    end(body?: RecognizedString) : HttpResponse;
     /** Ends this response, or tries to, by streaming appropriately sized chunks of body. Use in conjunction with onWritable. Returns tuple [ok, hasResponded].*/
-    tryEnd(fullBodyOrChunk: string, totalSize: number) : [boolean, boolean];
+    tryEnd(fullBodyOrChunk: RecognizedString, totalSize: number) : [boolean, boolean];
 
     /** Immediately force closes the connection. */
     close() : HttpResponse;
@@ -52,7 +61,7 @@ interface HttpResponse {
      * You MUST return true for success, false for failure.
      * Writing nothing is always success, so by default you must return true.
      */
-    onWritable(handler: (res: HttpResponse) => boolean) : HttpResponse;
+    onWritable(handler: (offset: number) => boolean) : HttpResponse;
 
     /** Every HttpResponse MUST have an attached abort handler IF you do not respond
      * to it immediately inside of the callback. Returning from an Http request handler
@@ -61,13 +70,19 @@ interface HttpResponse {
     onAborted(handler: (res: HttpResponse) => void) : HttpResponse;
 
     /** Handler for reading data from POST and such requests. You MUST copy the data of chunk if isLast is not true. We Neuter ArrayBuffers on return, making it zero length.*/
-    onData(handler: (res: HttpResponse, chunk: ArrayBuffer, isLast: boolean) => void) : HttpResponse;
+    onData(handler: (chunk: ArrayBuffer, isLast: boolean) => void) : HttpResponse;
+
+    /** Returns the remote IP address */
+    getRemoteAddress() : ArrayBuffer;
+
+    /** Arbitrary user data may be attached to this object */
+    [key: string]: any;
 }
 
 /** An HttpRequest is stack allocated and only accessible during the callback invocation. */
 interface HttpRequest {
     /** Returns the lowercased header value or empty string. */
-    getHeader(lowerCaseKey: string) : string;
+    getHeader(lowerCaseKey: RecognizedString) : string;
     /** Returns the parsed parameter at index. Corresponds to route. */
     getParameter(index: number) : string;
     /** Returns the URL including initial /slash */
@@ -76,6 +91,8 @@ interface HttpRequest {
     getMethod() : string;
     /** Returns the part of URL after ? sign or empty string. */
     getQuery() : string;
+    /** Loops over all headers. */
+    forEach(cb: (key: string, value: string) => void);
 }
 
 /** A structure holding settings and handlers for a WebSocket route handler. */
@@ -85,7 +102,7 @@ interface WebSocketBehavior {
     /** Maximum amount of seconds that may pass without sending or getting a message. */
     idleTimeout?: number;
     /** 0 = no compression, 1 = shared compressor, 2 = dedicated compressor. See C++ project. */
-    compression?: number;
+    compression?: CompressOptions;
     /** Handler for new WebSocket connection. WebSocket is valid from open to close, no errors. */
     open?: (ws: WebSocket, req: HttpRequest) => void;
     /** Handler for a WebSocket message. */
@@ -98,40 +115,42 @@ interface WebSocketBehavior {
 
 /** Options used when constructing an app. */
 interface AppOptions {
-    key_file_name?: string;
-    cert_file_name?: string;
-    passphrase?: string;
-    dh_params_file_name?: string;
+    key_file_name?: RecognizedString;
+    cert_file_name?: RecognizedString;
+    passphrase?: RecognizedString;
+    dh_params_file_name?: RecognizedString;
+    /** This translates to SSL_MODE_RELEASE_BUFFERS */
+    ssl_prefer_low_memory_usage?: boolean;
 }
 
 /** TemplatedApp is either an SSL or non-SSL app. */
 interface TemplatedApp {
     /** Listens to hostname & port. Callback hands either false or a listen socket. */
-    listen(host: string, port: number, cb: (listenSocket: us_listen_socket) => void): TemplatedApp;
+    listen(host: RecognizedString, port: number, cb: (listenSocket: us_listen_socket) => void): TemplatedApp;
     /** Listens to port. Callback hands either false or a listen socket. */
     listen(port: number, cb: (listenSocket: any) => void): TemplatedApp;
     /** Registers an HTTP GET handler matching specified URL pattern. */
-    get(pattern: string, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
+    get(pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
     /** Registers an HTTP POST handler matching specified URL pattern. */
-    post(pattern: string, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
+    post(pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
     /** Registers an HTTP OPTIONS handler matching specified URL pattern. */
-    options(pattern: string, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
+    options(pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
     /** Registers an HTTP DELETE handler matching specified URL pattern. */
-    del(pattern: string, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
+    del(pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
     /** Registers an HTTP PATCH handler matching specified URL pattern. */
-    patch(pattern: string, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
+    patch(pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
     /** Registers an HTTP PUT handler matching specified URL pattern. */
-    put(pattern: string, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
+    put(pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
     /** Registers an HTTP HEAD handler matching specified URL pattern. */
-    head(pattern: string, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
+    head(pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
     /** Registers an HTTP CONNECT handler matching specified URL pattern. */
-    connect(pattern: string, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
+    connect(pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
     /** Registers an HTTP TRACE handler matching specified URL pattern. */
-    trace(pattern: string, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
+    trace(pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
     /** Registers an HTTP handler matching specified URL pattern on any HTTP method. */
-    any(pattern: string, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
+    any(pattern: RecognizedString, handler: (res: HttpResponse, req: HttpRequest) => void) : TemplatedApp;
     /** Registers a handler matching specified URL pattern where WebSocket upgrade requests are caught. */
-    ws(pattern: string, behavior: WebSocketBehavior) : TemplatedApp;
+    ws(pattern: RecognizedString, behavior: WebSocketBehavior) : TemplatedApp;
 }
 
 /** Constructs a non-SSL app */

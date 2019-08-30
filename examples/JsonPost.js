5d72d66c6402ece3f654373552750b7834765161
Andrey Sakharov <a.sakharov@biletix.ru>
2019-08-30 04:23:26 +0300

    JsonPost \example code minimizing

--- examples/JsonPost.js
+++ examples/JsonPost.js
@@ -33,36 +33,19 @@ const app = uWS./*SSL*/App({
 
 /* Helper function for reading a posted JSON body */
 function readJson(res, cb, err) {
-  let buffer;
+  let buffer = Buffer.from('');
   /* Register data cb */
   res.onData((ab, isLast) => {
-    let chunk = Buffer.from(ab);
+    const chunk = Buffer.from(ab);
+    buffer = Buffer.concat([buffer, chunk]);
     if (isLast) {
-      let json;
-      if (buffer) {
-        try {
-          json = JSON.parse(Buffer.concat([buffer, chunk]));
-        } catch (e) {
-          /* res.close calls onAborted */
-          res.close();
-          return;
-        }
+      try {
+        const json = JSON.parse(buffer.toString());
         cb(json);
-      } else {
-        try {
-          json = JSON.parse(chunk);
-        } catch (e) {
-          /* res.close calls onAborted */
-          res.close();
-          return;
-        }
-        cb(json);
-      }
-    } else {
-      if (buffer) {
-        buffer = Buffer.concat([buffer, chunk]);
-      } else {
-        buffer = Buffer.concat([chunk]);
+      } catch (e) {
+        /* res.close calls onAborted */
+        res.close();
+        return;
       }
     }
   });

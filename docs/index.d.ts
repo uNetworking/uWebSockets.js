a30e921b72d00ee33120a3cebaf0ca06dee58bb5
Arnaud Benhamdine <abenhamdine@users.noreply.github.com>
2019-10-10 21:08:22 +0200

    Remove res argument in callback

--- docs/index.d.ts
+++ docs/index.d.ts
@@ -67,7 +67,7 @@ interface HttpResponse {
      * to it immediately inside of the callback. Returning from an Http request handler
      * without attaching (by calling onAborted) an abort handler is ill-use and will termiante.
      * When this event emits, the response has been aborted and may not be used. */
-    onAborted(handler: (res: HttpResponse) => void) : HttpResponse;
+    onAborted(handler: () => void) : HttpResponse;
 
     /** Handler for reading data from POST and such requests. You MUST copy the data of chunk if isLast is not true. We Neuter ArrayBuffers on return, making it zero length.*/
     onData(handler: (chunk: ArrayBuffer, isLast: boolean) => void) : HttpResponse;

c32c147aef6baa7e7ec4c07af2589f1a0847ffcb
Ido <idofilus@gmail.com>
2020-06-03 20:15:14 +0300

    Added bytes concat of the chunks
    Unlike this JsonPost.js example which cause the bytes to look wrong on file ~80KB, this will also create the buffer once.
    As for larger file uploads I think streaming to the file system similar to shown in VideoStreamer.js would be better.

--- examples/Upload.js
+++ examples/Upload.js
@@ -13,13 +13,19 @@ const app = uWS./*SSL*/App({
   passphrase: '1234'
 }).post('/*', (res, req) => {
   console.log('Posted to ' + req.getUrl());
+  const contentLength = +req.getHeader("content-length");
+  const bytes = new Uint8Array(contentLength);
+  let offset = 0;
   res.onData((chunk, isLast) => {
     /* Buffer this anywhere you want to */
     console.log('Got chunk of data with length ' + chunk.byteLength + ', isLast: ' + isLast);
+    bytes.set(new Uint8Array(chunk), offset);
+    offset += chunk.byteLength;
 
     /* We respond when we are done */
     if (isLast) {
       res.end('Thanks for the data!');
+      /* bytes contains the whole file as Uint8Array, you can get the buffer with Buffer.from(bytes)  */
     }
   });
 

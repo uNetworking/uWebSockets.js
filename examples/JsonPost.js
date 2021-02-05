4e87f33b914328310f56ddb37060ac0027f85f6f
krm35 <49982065+krm35@users.noreply.github.com>
2021-02-05 22:40:47 +0100

    Refacto readJson

--- examples/JsonPost.js
+++ examples/JsonPost.js
@@ -33,40 +33,22 @@ const app = uWS./*SSL*/App({
 
 /* Helper function for reading a posted JSON body */
 function readJson(res, cb, err) {
-  let buffer;
-  /* Register data cb */
-  res.onData((ab, isLast) => {
-    let chunk = Buffer.from(ab);
-    if (isLast) {
-      let json;
-      if (buffer) {
-        try {
-          json = JSON.parse(Buffer.concat([buffer, chunk]));
-        } catch (e) {
-          /* res.close calls onAborted */
-          res.close();
-          return;
+    let buffer;
+    /* Register data cb */
+    res.onData((ab, isLast) => {
+        let chunk = Buffer.from(ab);
+        if (isLast) {
+            try {
+                cb(JSON.parse((buffer ? Buffer.concat([buffer, chunk]) : chunk)));
+            } catch (e) {
+                /* res.close calls onAborted */
+                res.close();
+            }
+        } else {
+            buffer = Buffer.concat(buffer ? [buffer, chunk] : [chunk]);
         }
-        cb(json);
-      } else {
-        try {
-          json = JSON.parse(chunk);
-        } catch (e) {
-          /* res.close calls onAborted */
-          res.close();
-          return;
-        }
-        cb(json);
-      }
-    } else {
-      if (buffer) {
-        buffer = Buffer.concat([buffer, chunk]);
-      } else {
-        buffer = Buffer.concat([chunk]);
-      }
-    }
-  });
+    });
 
-  /* Register error cb */
-  res.onAborted(err);
+    /* Register error cb */
+    res.onAborted(err);
 }

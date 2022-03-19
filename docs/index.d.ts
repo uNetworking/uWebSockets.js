a12855923ea0abd7cd6fec49b85ddc08047599a2
Matt Moran <mattmoran2000@gmail.com>
2022-03-19 21:30:00 +1300

    Type `us_listen_socket extends us_socket`
    See https://github.com/uNetworking/uSockets/blob/0128479ed83eb55e18542a96535dd15ef09a3aaf/src/internal/internal.h#L106

--- docs/index.d.ts
+++ docs/index.d.ts
@@ -15,7 +15,7 @@
 /** Native type representing a raw uSockets struct us_listen_socket_t.
  * Careful with this one, it is entirely unchecked and native so invalid usage will blow up.
  */
-export interface us_listen_socket {
+export interface us_listen_socket extends us_socket {
 
 }
 

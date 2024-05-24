90829d176c4f927e004f652fc36f06c29859ed8c
Alex Hultman <alexhultman@gmail.com>
2024-05-24 09:20:08 +0000

    [GitHub Actions] Updated ubuntu-20.04 binaries

--- uws.js
+++ uws.js
@@ -22,3 +22,40 @@ module.exports = (() => {
 		throw new Error('This version of uWS.js supports only Node.js LTS versions 16, 18 and 20 on (glibc) Linux, macOS and Windows, on Tier 1 platforms (https://github.com/nodejs/node/blob/master/BUILDING.md#platform-list).\n\n' + e.toString());
 	}
 })();
+
+module.exports.DeclarativeResponse = class DeclarativeResponse {
+  constructor() {
+    this.instructions = [];
+  }
+
+  // Utility method to encode text and append instruction
+  _appendInstruction(opcode, ...text) {
+    this.instructions.push(opcode);
+    text.forEach(str => {
+      const bytes = new TextEncoder().encode(str);
+      this.instructions.push(bytes.length, ...bytes);
+    });
+  }
+
+  // Utility method to append 2-byte length text in little-endian format
+  _appendInstructionWithLength(opcode, text) {
+    this.instructions.push(opcode);
+    const bytes = new TextEncoder().encode(text);
+    const length = bytes.length;
+    this.instructions.push(length & 0xff, (length >> 8) & 0xff, ...bytes);
+  }
+
+  writeHeader(key, value) { return this._appendInstruction(1, key, value), this; }
+  writeBody() { return this.instructions.push(2), this; }
+  writeQueryValue(key) { return this._appendInstruction(3, key), this; }
+  writeHeaderValue(key) { return this._appendInstruction(4, key), this; }
+  write(value) { return this._appendInstructionWithLength(5, value), this; }
+  writeParameterValue(key) { return this._appendInstruction(6, key), this; }
+
+  end(value) {
+    const bytes = new TextEncoder().encode(value);
+    const length = bytes.length;
+    this.instructions.push(0, length & 0xff, (length >> 8) & 0xff, ...bytes);
+    return new Uint8Array(this.instructions).buffer;
+  }
+}

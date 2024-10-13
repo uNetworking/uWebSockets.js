3d86c7995e1e6c49e9c3e42f6c8cd9c15fe0b4f8
rtritto <ariele.1994@gmail.com>
2024-10-13 13:17:57 +0200

    Change var to type in index.d.ts

--- docs/index.d.ts
+++ docs/index.d.ts
@@ -360,40 +360,40 @@ export function getParts(body: RecognizedString, contentType: RecognizedString):
 /** WebSocket compression options. Combine any compressor with any decompressor using bitwise OR. */
 export type CompressOptions = number;
 /** No compression (always a good idea if you operate using an efficient binary protocol) */
-export var DISABLED: CompressOptions;
+export type DISABLED = CompressOptions;
 /** Zero memory overhead compression. */
-export var SHARED_COMPRESSOR: CompressOptions;
+export type SHARED_COMPRESSOR = CompressOptions;
 /** Zero memory overhead decompression. */
-export var SHARED_DECOMPRESSOR: CompressOptions;
+export type SHARED_DECOMPRESSOR = CompressOptions;
 /** Sliding dedicated compress window, requires 3KB of memory per socket */
-export var DEDICATED_COMPRESSOR_3KB: CompressOptions;
+export type DEDICATED_COMPRESSOR_3KB = CompressOptions;
 /** Sliding dedicated compress window, requires 4KB of memory per socket */
-export var DEDICATED_COMPRESSOR_4KB: CompressOptions;
+export type DEDICATED_COMPRESSOR_4KB = CompressOptions;
 /** Sliding dedicated compress window, requires 8KB of memory per socket */
-export var DEDICATED_COMPRESSOR_8KB: CompressOptions;
+export type DEDICATED_COMPRESSOR_8KB = CompressOptions;
 /** Sliding dedicated compress window, requires 16KB of memory per socket */
-export var DEDICATED_COMPRESSOR_16KB: CompressOptions;
+export type DEDICATED_COMPRESSOR_16KB = CompressOptions;
 /** Sliding dedicated compress window, requires 32KB of memory per socket */
-export var DEDICATED_COMPRESSOR_32KB: CompressOptions;
+export type DEDICATED_COMPRESSOR_32KB = CompressOptions;
 /** Sliding dedicated compress window, requires 64KB of memory per socket */
-export var DEDICATED_COMPRESSOR_64KB: CompressOptions;
+export type DEDICATED_COMPRESSOR_64KB = CompressOptions;
 /** Sliding dedicated compress window, requires 128KB of memory per socket */
-export var DEDICATED_COMPRESSOR_128KB: CompressOptions;
+export type DEDICATED_COMPRESSOR_128KB = CompressOptions;
 /** Sliding dedicated compress window, requires 256KB of memory per socket */
-export var DEDICATED_COMPRESSOR_256KB: CompressOptions;
+export type DEDICATED_COMPRESSOR_256KB = CompressOptions;
 /** Sliding dedicated decompress window, requires 32KB of memory per socket (plus about 23KB) */
-export var DEDICATED_DECOMPRESSOR_32KB: CompressOptions;
+export type DEDICATED_DECOMPRESSOR_32KB = CompressOptions;
 /** Sliding dedicated decompress window, requires 16KB of memory per socket (plus about 23KB) */
-export var DEDICATED_DECOMPRESSOR_16KB: CompressOptions;
+export type DEDICATED_DECOMPRESSOR_16KB = CompressOptions;
 /** Sliding dedicated decompress window, requires 8KB of memory per socket (plus about 23KB) */
-export var DEDICATED_DECOMPRESSOR_8KB: CompressOptions;
+export type DEDICATED_DECOMPRESSOR_8KB = CompressOptions;
 /** Sliding dedicated decompress window, requires 4KB of memory per socket (plus about 23KB) */
-export var DEDICATED_DECOMPRESSOR_4KB: CompressOptions;
+export type DEDICATED_DECOMPRESSOR_4KB = CompressOptions;
 /** Sliding dedicated decompress window, requires 2KB of memory per socket (plus about 23KB) */
-export var DEDICATED_DECOMPRESSOR_2KB: CompressOptions;
+export type DEDICATED_DECOMPRESSOR_2KB = CompressOptions;
 /** Sliding dedicated decompress window, requires 1KB of memory per socket (plus about 23KB) */
-export var DEDICATED_DECOMPRESSOR_1KB: CompressOptions;
+export type DEDICATED_DECOMPRESSOR_1KB = CompressOptions;
 /** Sliding dedicated decompress window, requires 512B of memory per socket (plus about 23KB) */
-export var DEDICATED_DECOMPRESSOR_512B: CompressOptions;
+export type DEDICATED_DECOMPRESSOR_512B = CompressOptions;
 /** Sliding dedicated decompress window, requires 32KB of memory per socket (plus about 23KB) */
-export var DEDICATED_DECOMPRESSOR: CompressOptions;
+export type DEDICATED_DECOMPRESSOR = CompressOptions;

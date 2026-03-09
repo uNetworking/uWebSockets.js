6c88ea5160f7cf39f7b3575849926c35d582e400
BV-WebDev <19169837+BV-WebDev@users.noreply.github.com>
2026-03-09 19:47:06 +0000

    Update JsonPost example

--- examples/JsonPost.js
+++ examples/JsonPost.js
@@ -39,31 +39,18 @@ function readJson(res, cb, err) {
     let chunk = Buffer.from(ab);
     if (isLast) {
       let json;
-      if (buffer) {
-        try {
-          json = JSON.parse(Buffer.concat([buffer, chunk]));
-        } catch (e) {
-          /* res.close calls onAborted */
-          res.close();
-          return;
-        }
-        cb(json);
-      } else {
-        try {
-          json = JSON.parse(chunk);
-        } catch (e) {
-          /* res.close calls onAborted */
-          res.close();
-          return;
-        }
-        cb(json);
+      try {
+        json = JSON.parse(buffer ? Buffer.concat([buffer, chunk]) : chunk);
+      } catch (e) {
+        /* res.close calls onAborted */
+        res.close();
+        return;
       }
+      cb(json);
+    } else if (buffer) {
+      buffer = Buffer.concat([buffer, chunk]);
     } else {
-      if (buffer) {
-        buffer = Buffer.concat([buffer, chunk]);
-      } else {
-        buffer = Buffer.concat([chunk]);
-      }
+      buffer = Buffer.concat([chunk]);
     }
   });
 

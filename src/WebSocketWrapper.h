fb05cc7ccf31696230ec69da46b846b508f8871d
uNetworkingAB <110806833+uNetworkingAB@users.noreply.github.com>
2026-03-15 01:55:59 +0100

    Fix send_fast_buffer to use correct opcode

--- src/WebSocketWrapper.h
+++ src/WebSocketWrapper.h
@@ -341,17 +341,16 @@ template <bool SSL>
 static uint32_t uWS_WebSocket_send_fast_buffer(v8::Local<v8::Object> receiver, 
                                                v8::Local<v8::Value> message, 
                                                bool isBinary, bool compress) {
+    printf("Hitting fast buffer path\n");
     auto *ws = (uWS::WebSocket<SSL, true, PerSocketData> *) receiver->GetAlignedPointerFromInternalField(0);
     if (!ws || !message->IsArrayBufferView()) return 0;
 
     auto array = message.As<v8::ArrayBufferView>();
     auto contents = array->Buffer()->GetBackingStore();
     char* data = static_cast<char*>(contents->Data()) + array->ByteOffset();
-
-    printf("Hitting fast buffer path\n");
     
     return ws->send(std::string_view(data, array->ByteLength()),
-                    uWS::OpCode::BINARY, compress);
+                    isBinary ? uWS::OpCode::BINARY : uWS::OpCode::TEXT, compress);
 }
 
     template <bool SSL>

a2e030f4229e1db3154fb38ca1f03e97ad0c0d82
BV-WebDev <19169837+BV-WebDev@users.noreply.github.com>
2026-03-26 16:36:23 +0000

    Add DataView support, update RecognizedString type

--- docs/index.d.ts
+++ docs/index.d.ts
@@ -47,10 +47,11 @@ export interface AppDescriptor {
  * This because we expect UTF-8, which is packed in 8-byte chars. JavaScript strings are UTF-16 internally meaning extra copies and reinterpretation are required.
  *
  * That's why all events pass data by ArrayBuffer and not JavaScript strings, as they allow zero-copy data passing.
- *
  * You can always do Buffer.from(arrayBuffer).toString(), but keeping things binary and as ArrayBuffer is preferred.
+ * 
+ * The ArrayBufferView type includes Node.js Buffer, DataView, and TypedArray (Uint8Array, Uint16Array, ...).
  */
-export type RecognizedString = string | ArrayBuffer | Uint8Array | Int8Array | Uint16Array | Int16Array | Uint32Array | Int32Array | Float32Array | Float64Array;
+export type RecognizedString = string | ArrayBuffer | SharedArrayBuffer | ArrayBufferView;
 
 /** A WebSocket connection that is valid from open to close event.
  * Read more about this in the user manual.
